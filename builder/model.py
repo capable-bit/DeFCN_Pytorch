8d417812ab539a9d7976d38d4aa3f95d4ee856b2
HaiLong Li <1404714097@qq.com>
2021-12-08 22:22:12 +0800

    init

--- builder/model.py
+++ builder/model.py
@@ -1,7 +1,7 @@
 from model.fcos import FCOS
 from builder.backbone import build_backbone
 from builder.fcoshead import build_fcoshead
-from defcn_pytorch.builder.shift_generator import build_shift_generator
+from builder.shift_generator import build_shift_generator
 
 # ---------model------------- #
 # 构建模型
